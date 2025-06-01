a2cbb88d96c92d81cfdaeeb1882310ba3ccc1772
JobraneLouati <louetiijobrane@gmail.com>
2025-06-01 20:42:12 +0100

    model

--- app.py
+++ app.py
@@ -10,10 +10,14 @@ model = joblib.load('random_forest_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.get_json()
-    df = pd.DataFrame([data])
-    prediction = model.predict(df)[0]
-    return jsonify({'prediction': int(prediction)})
+    try:
+        data = request.get_json()
+        df = pd.DataFrame([data])
+        prediction = model.predict(df)[0]
+        return jsonify({'prediction': int(prediction)})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
